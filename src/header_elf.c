a2a030929bedbe922cd11107db828b970a98c763
Vincent <vincent.givaudan@hotmail.fr>
2017-01-03 14:59:31 +0100

    affichage du header

---
+++ src/header_elf.c
@@ -0,0 +1,289 @@
+#include <stdlib.h>
+
+#include "util.h"
+#include "elf_header.h"
+
+int display_header(Elf32_Ehdr *elf_header) {
+
+    int i = 0;
+
+    if (elf_header == NULL) {
+        fprintf(stderr, "Header is NULL\n");
+        return -1;
+    }
+
+    if (   elf_header->e_ident[EI_MAG0] != ELFMAG0
+        || elf_header->e_ident[EI_MAG1] != ELFMAG1
+        || elf_header->e_ident[EI_MAG2] != ELFMAG2
+        || elf_header->e_ident[EI_MAG3] != ELFMAG3
+        || elf_header->e_ident[EI_CLASS] != ELFCLASS32)
+    {
+        fprintf(stderr, "Could not print header: not a 32bits elf file\n");
+        return -1;
+    }
+
+    // Start print header
+    printf("╔════════════════════════════════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                                             \e[1;31mELF header:\e[0m                                                ║\n");
+    printf("╠════════════════════════════════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║ Magic number:%-42s", "");
+    for (i = 0; i < EI_NIDENT; i++) {
+        printf("%2.2x ", elf_header->e_ident[i]);
+    }
+    printf("║\n╟────────────────────────────────────────────────────────┬───────────────────────────────────────────────╢\n");
+
+    printf("║ File Class:  %-41s │ %-46s║\n", "", get_file_class(elf_header->e_ident[EI_CLASS]));
+
+    printf("║ Data encoding:  %-38s │ %-46s║\n", "", get_data_encoding(elf_header->e_ident[EI_DATA]));
+
+    printf("║ ELF header version number:  %-26s │ %-46s║\n", "", get_header_version(elf_header->e_ident[EI_VERSION]));
+
+    printf("║ Operating system:  %-35s │ %-46s║\n", "", get_operating_system(elf_header->e_ident[EI_OSABI]));
+
+    printf("║ ABI Version :  %-39s │ %-46d║\n", "", elf_header->e_ident[EI_ABIVERSION]);
+
+    printf("║ Object file type:  %-35s │ %-46s║\n", "", get_object_file_type(elf_header->e_type));
+
+    printf("║ Required architecture (machine):  %-20s │ %-46s║\n", "", get_architecture(elf_header->e_machine));
+
+    printf("║ Object file version:   %-31s │ %-46s║\n", "", get_object_file_version(elf_header->e_version));
+
+    printf("║ Entry point:  %-40s │ ", "");
+    if (elf_header->e_entry == 0) printf("No associated entry point (0x%x)               ║\n", elf_header->e_entry);
+    else printf("%#-48x║\n", elf_header->e_entry);
+
+    printf("║ Program header table's file offset (bytes):  %-9s │ ", "");
+    if (elf_header->e_phoff == 0) printf("0 (No program header table)                   ║\n");
+    else printf("%-48d║\n", elf_header->e_phoff);
+
+    printf("║ Section header table's file offset (bytes):  %-9s │ ", "");
+    if (elf_header->e_shoff == 0) printf("0 (No section header table)           ║\n");
+    else printf("%-46d║\n", elf_header->e_shoff);
+
+    printf("║ Flags:  %-46s │ %#-46x║\n", "", elf_header->e_flags);
+
+    printf("║ ELF header's size (bytes):  %-26s │ %-46d║\n", "", elf_header->e_ehsize);
+
+    printf("║ Size of one entry in the program header table (bytes): │ %-46d║\n", elf_header->e_phentsize);
+
+    printf("║ Number of entries in the program header table:         │ ");
+    if (elf_header->e_phnum == 0) printf("0 (No program header table)                   ║\n");
+    else printf("%-48d║\n", elf_header->e_phnum);
+
+    printf("║ Size of one entry in the section header table (bytes): │ %-46d║\n", elf_header->e_shentsize);
+
+    printf("║ Number of entries in the section header table:         │ ", "");
+    if (elf_header->e_shnum == 0) printf("0 (No section header table)                ║\n");
+    else printf("%-46d║\n", elf_header->e_shnum);
+
+    printf("║ Section header table index:  %-25s │ %-46d║\n", "", elf_header->e_shstrndx);
+    // End print header
+    printf("╚════════════════════════════════════════════════════════╧═══════════════════════════════════════════════╝\n");
+    return 1;
+ }
+
+int read_header(ELF_STRUCT* elf_struct) {
+
+    int reverse_needed;
+    int header_size = sizeof(Elf32_Ehdr);
+
+    // Header is at the beginning of the file
+    fseek(elf_struct->elf_file, 0, SEEK_SET);
+
+    // We read the whole header in one fread
+    if ( fread(elf_struct->elf_header, header_size, 1, elf_struct->elf_file) == -1 ) {
+        return -1;
+    }
+
+    // big endian / little endian
+    reverse_needed = this_needs_reverse( elf_struct->elf_header->e_ident[EI_DATA] );
+
+    if (reverse_needed) {
+
+        elf_struct->elf_header->e_type = reverse_2(elf_struct->elf_header->e_type);
+        elf_struct->elf_header->e_machine = reverse_2(elf_struct->elf_header->e_machine);
+        elf_struct->elf_header->e_version = reverse_4(elf_struct->elf_header->e_version);
+        elf_struct->elf_header->e_entry = reverse_4(elf_struct->elf_header->e_entry);
+        elf_struct->elf_header->e_phoff = reverse_4(elf_struct->elf_header->e_phoff);
+        elf_struct->elf_header->e_shoff = reverse_4(elf_struct->elf_header->e_shoff);
+        elf_struct->elf_header->e_flags = reverse_4(elf_struct->elf_header->e_flags);
+        elf_struct->elf_header->e_ehsize = reverse_2(elf_struct->elf_header->e_ehsize);
+        elf_struct->elf_header->e_phentsize = reverse_2(elf_struct->elf_header->e_phentsize);
+        elf_struct->elf_header->e_phnum = reverse_2(elf_struct->elf_header->e_phnum);
+        elf_struct->elf_header->e_shentsize = reverse_2(elf_struct->elf_header->e_shentsize);
+        elf_struct->elf_header->e_shnum = reverse_2(elf_struct->elf_header->e_shnum);
+        elf_struct->elf_header->e_shstrndx = reverse_2(elf_struct->elf_header->e_shstrndx);
+
+    }
+
+    return 1;
+}
+
+char* get_file_class(unsigned char ei_class) {
+
+    switch (ei_class) {
+
+        case ELFCLASSNONE:  return "Invalid Class";
+        case ELFCLASS32:    return "32-bit objects";
+        case ELFCLASS64:    return "64-bit objects";
+        default:            return "Unknown";
+    }
+
+}
+
+char* get_data_encoding(unsigned char ei_data) {
+
+    switch (ei_data) {
+
+        case ELFDATANONE:   return "Invalid data encoding";
+        case ELFDATA2LSB:   return "2's complement values, little endian";
+        case ELFDATA2MSB:   return "2's complement, big endian";
+        default:            return "Unknown";
+
+    }
+
+}
+
+char* get_header_version(unsigned char ei_version) {
+
+    switch (ei_version) {
+
+        case EV_NONE:       return "0 (invalid version)";
+        case EV_CURRENT:    return "1 (current version)";
+        default:            return "Unknown";
+
+    }
+
+}
+
+char* get_operating_system(unsigned char ei_osabi) {
+
+    switch(ei_osabi) {
+
+        case ELFOSABI_SYSV:         return "Unix System V";
+        case ELFOSABI_HPUX:         return "HP-UX";
+        case ELFOSABI_NETBSD:       return "NetBSD";
+        case ELFOSABI_LINUX:        return "Linux";
+        case ELFOSABI_SOLARIS:      return "Solaris";
+        case ELFOSABI_IRIX:         return "Irix";
+        case ELFOSABI_FREEBSD:      return "FreeBSD";
+        case ELFOSABI_TRU64:        return "TRU64 Unix";
+        case ELFOSABI_ARM:          return "ARM architecture";
+        case ELFOSABI_STANDALONE:   return "Stand-Alone (embedded)";
+        default:                    return "Unknown";
+
+    }
+
+}
+
+char* get_object_file_type(Elf64_Half e_type) {
+
+    switch(e_type) {
+
+        case ET_NONE:   return "No File Type";
+        case ET_REL:    return "Relocatable File";
+        case ET_EXEC:   return "Executable File";
+        case ET_DYN:    return "Shared Object File";
+        case ET_CORE:   return "Core File";
+        case ET_LOPROC: return "Processor Specific";
+        case ET_HIPROC: return "Processor Specific";
+        default:        return "Unknown";
+
+    }
+
+}
+
+char* get_architecture(Elf64_Half e_machine) {
+
+    switch(e_machine) {
+
+        case EM_NONE:           return "No machine";
+        case EM_SPARC:          return "Sun SPARC";
+        case EM_386:            return "Intel 80386";
+        case EM_68K:            return "Motorola 68000";
+        case EM_88K:            return "Motorola 88000";
+        case EM_860:            return "Intel i860";
+        case EM_MIPS:           return "MIPS RS3000 Big-Endian";
+        case EM_S370:           return "IBM System/370 Processor";
+        case EM_MIPS_RS3_LE:    return "MIPS RS3000 Little-Endian";
+        case EM_PARISC:         return "PA-RISC";
+        case EM_VPP500:         return "Fujitsu VPP500";
+        case EM_SPARC32PLUS:    return "Sun SPARC 32+";
+        case EM_960:            return "Intel 80960";
+        case EM_PPC:            return "PowerPC";
+        case EM_PPC64:          return "64-bit PowerPC";
+        case EM_S390:           return "IBM System/390 Processor";
+        case EM_V800:           return "NEX V800";
+        case EM_FR20:           return "Fujitsu FR20";
+        case EM_RH32:           return "TRW RH-32";
+        case EM_RCE:            return "Motorola RCE";
+        case EM_ARM:            return "Advanced RISC Machines ARM";
+        case EM_ALPHA:          return "Digital Alpha";
+        case EM_SH:             return "Hitachi SH";
+        case EM_SPARCV9:        return "Sun SPARC V9 (64-bit)";
+        case EM_TRICORE:        return "Siemens Tricore embedded processor";
+        case EM_ARC:            return "Argonaut RISC Core";
+        case EM_H8_300:         return "Hitachi H8/300";
+        case EM_H8_300H:        return "Hitachi H8/300H";
+        case EM_H8S:            return "Hitachi H8S";
+        case EM_H8_500:         return "Hitachi H8/500";
+        case EM_IA_64:          return "Intel IA64";
+        case EM_MIPS_X:         return "Stanford MIPS-X";
+        case EM_COLDFIRE:       return "Motorola ColdFire";
+        case EM_68HC12:         return "Motorola M68HC12";
+        case EM_MMA:            return "Fujitsu MMA Mulimedia Accelerator";
+        case EM_PCP:            return "Siemens PCP";
+        case EM_NCPU:           return "Sony nCPU embedded RISC processor";
+        case EM_NDR1:           return "Denso NDR1 microprocessor";
+        case EM_STARCORE:       return "Motorola Star*Core processor";
+        case EM_ME16:           return "Toyota ME16 processor";
+        case EM_ST100:          return "STMicroelectronics ST100 processor";
+        case EM_TINYJ:          return "Advanced Logic Corp. TinyJ";
+        case EM_X86_64:         return "AMDs x86-64 architecture";
+        case EM_PDSP:           return "Sony DSP Processor";
+        case EM_FX66:           return "Siemens FX66 microcontroller";
+        case EM_ST9PLUS:        return "STMicroelectronics ST9+8/16 bit";
+        case EM_ST7:            return "STMicroelectronics ST7 8-bit";
+        case EM_68HC16:         return "Motorola MC68HC16 Microcontroller";
+        case EM_68HC11:         return "Motorola MC68HC11 Microcontroller";
+        case EM_68HC08:         return "Motorola MC68HC08 Microcontroller";
+        case EM_68HC05:         return "Motorola MC68HC05 Microcontroller";
+        case EM_SVX:            return "Silicon Graphics SVx";
+        case EM_ST19:           return "STMicroelectronics ST19 8-bit";
+        case EM_VAX:            return "Digital VAX";
+        case EM_CRIS:           return "Axis Communications 32-bit";
+        case EM_JAVELIN:        return "Infineon Technologies 32-bit";
+        case EM_FIREPATH:       return "Element 14 64-bit DSP Processor";
+        case EM_ZSP:            return "LSI Logic 16-bit DSP Processor";
+        case EM_MMIX:           return "Donald Knuth's educational";
+        case EM_HUANY:          return "Harvard University";
+        case EM_PRISM:          return "SiTera Prism";
+        case EM_AVR:            return "Atmel AVR 8-bit microcontroller";
+        case EM_FR30:           return "Fujitsu FR30";
+        case EM_D10V:           return "Mitsubishi D10V";
+        case EM_D30V:           return "Mitsubishi D30V";
+        case EM_V850:           return "NEC v850";
+        case EM_M32R:           return "Mitsubishi M32R";
+        case EM_MN10300:        return "Matsushita MN10300";
+        case EM_MN10200:        return "Matsushita MN10200";
+        case EM_PJ:             return "picoJava";
+        case EM_OPENRISC:       return "OpenRISC 32-bit embedded processor";
+        case EM_ARC_A5:         return "ARC Cores Tangent-A5";
+        case EM_XTENSA:         return "Tensilica Xtensa architecture";
+        default:                return "Unknown";
+
+    }
+
+}
+
+char* get_object_file_version(Elf32_Word e_version) {
+
+    switch (e_version) {
+
+        case EV_NONE:       return "0 (invalid version)";
+        case EV_CURRENT:    return "1 (current version)";
+        default:            return "Unknown";
+
+    }
+
+}
---
+++ src/header_elf.h
@@ -0,0 +1,110 @@
+/**
+  * @file elf_header.h
+  *	@date 8 Jan 2016
+  *	@brief Module used to read and display an ELF header.
+  */
+
+#ifndef __ELF_HEADER_H__
+#define __ELF_HEADER_H__
+
+#include <elf.h>
+#include <stdio.h>
+#include "elf_struct.h"
+/**
+  * @brief Displays information from an ELF header.
+  *
+  *	Prints in that particular order:
+  *	- Magic Number
+  *	- File Class
+  *	- Data Encoding
+  *	- Elf header Version Number
+  *	- Operating system and ABI
+  *	- ABI Version
+  *	- Object file type
+  *	- Required architecture
+  *	- Object file version
+  *	- Entry point (starting the process)
+  *	- header table's file offset
+  *	- section table's file offset
+  *	- flags
+  *	- Header's size
+  *	- Size of one entry in the file's program header table
+  *	- Number of entries in the file's program header table
+  *	- Size of one entry in the file's section header table
+  *	- Number of entries in the file's section header table
+  *	- section header table index of the entry associated
+  *	  with the section name string table
+  * @param elf_header The header to be displayed.
+  *	@return -1 if not a 32bits ELF header.
+  * @return 1 otherwhise.
+  */
+int display_header(Elf32_Ehdr *elf_header);
+
+/**
+  * @brief Gets the human-readeable file's class, or capacity.
+  *
+  *	@param ei_class The Elf32_Ehdr->e_ident[] member identifying the file's class, or capacity.
+  * @return String containing the human-readeable file's class, or capacity.
+  */
+char* get_file_class(unsigned char ei_class);
+
+/**
+  * @brief Gets the human-readeable data encoding.
+  *
+  *	@param ei_data The Elf32_Ehdr->e_ident[] member identifying the data encoding.
+  * @return String containing the human-readeable data encoding.
+  */
+char* get_data_encoding(unsigned char ei_data);
+
+/**
+  * @brief Gets the human-readeable ELF header version number.
+  *
+  *	@param ei_version The Elf32_Ehdr->e_ident[] member identifying the ELF header version number.
+  * @return String containing the human-readeable ELF header version number.
+  */
+char* get_header_version(unsigned char ei_version);
+
+
+/**
+  * @brief Gets the human-readeable operating system.
+  *
+  *	@param ei_osabi The Elf32_Ehdr->e_ident[] member identifying the operating system.
+  * @return String containing the human-readeable operating system.
+  */
+char* get_operating_system(unsigned char ei_osabi);
+
+/**
+  * @brief Gets the human-readeable object file type.
+  *
+  *	@param e_type The Elf32_Ehdr member identifying the object file type.
+  * @return String containing the human-readeable object file type.
+  */
+char* get_object_file_type(Elf64_Half e_type);
+
+/**
+  * @brief Gets the human-readeable required architecture for an individual file.
+  *
+  *	@param e_machine The Elf32_Ehdr member identifying the required architecture for an individual file.
+  * @return String containing the human-readeable required architecture for an individual file.
+  */
+char* get_architecture(Elf64_Half e_machine);
+
+/**
+  * @brief Gets the human-readeable object file version.
+  *
+  *	@param e_version The Elf32_Ehdr member identifying the object file version.
+  * @return String containing the human-readeable object file version.
+  */
+char* get_object_file_version(Elf32_Word e_version);
+
+/**
+  * @brief Reads data of an ELF header from a given file and puts the data into a given header struct.
+  *
+  * @param f The file we're reading from. The file must have been opened beforehand.
+  * @param elf_header The header struct to be filled. Memory must have been allocated beforehand.
+  * @return -1 if fread error.
+  * @return  1 otherwise.
+  */
+int read_header(ELF_STRUCT* elf_struct);
+
+#endif
