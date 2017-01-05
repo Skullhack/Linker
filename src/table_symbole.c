a00542779a3b625447ac2d9cc375a8c0a68bd18d
BananeGuimauve <florian.gourdeau@gmail.com>
2017-01-05 10:44:10 +0100

    modif table_symbole.c

--- src/table_symbole.c
+++ src/table_symbole.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <elf.h>
 
-int creer_table_symbole(GLOBAL_STRUCT * elf){
+int creer_table_symbole(ELF_STRUCT * elf){
 	//table_symboles tab_symb;
 	//uint16_t taille_sections;
 	int reverse_needed;
@@ -26,30 +26,26 @@ int creer_table_symbole(GLOBAL_STRUCT * elf){
 
 	//taille_sections = header->e_shnum * header->e_shentsize;
 	
-	fseek(f, taille_tab_symb, adr_tab_symb);
+	fseek(elf->elf_file, taille_tab_symb, adr_tab_symb);
 	
 	nb_symb = taille_tab_symb / sizeof(Elf32_Sym);
 	
-	// We read the symbole table one by one
-	
+	// We read the symbole table one by one	
 	for (i=0; i<nb_symb;i++){
-   		if ( fread(&(elf.*a_sym[i]), sizeof(Elf32_Sym), 1, f) == -1 ) {
+   		if ( fread(&(elf.*a_sym[i]), sizeof(Elf32_Sym), 1, elf->elf_file) == -1 ) {
         	return -1;
     	}
     }    
     
-    // A CHANGER A PARTIR D'ICI !!!
     // big endian / little endian
     reverse_needed = need_reverse( elf.elf_header->e_ident[EI_DATA] );
 
     if (reverse_needed) {    
     	for (i=0; i<nb_symb;i++){
-
 		    elf.a_sym[i]->st_name = reverse_4(elf.a_sym[i]->st_name);
 		    elf.a_sym[i]->st_value = reverse_4(elf.a_sym[i]->st_value);
 		    elf.a_sym[i]->st_size = reverse_4(elf.a_sym[i]->st_size);
-		    elf.a_sym[i]->st_shndx = reverse_2(elf.a_sym[i]->st_shndx);
-		    
+		    elf.a_sym[i]->st_shndx = reverse_2(elf.a_sym[i]->st_shndx);		    
 		}
     }
 
@@ -58,10 +54,8 @@ int creer_table_symbole(GLOBAL_STRUCT * elf){
 
 void afficher_table(Elf32_Sym tab_symb){
 	
-	for (i=0; i<nb_symb;i++){
-	
-		printf("Le st_name de la table des symboles %d est : %d\n", elf.a_sym[i]->st_name);
-	
+	for (i=0; i<nb_symb;i++){	
+		printf("Le st_name de la table des symboles %d est : %d\n", elf.a_sym[i]->st_name);	
 	}
 	
 }
