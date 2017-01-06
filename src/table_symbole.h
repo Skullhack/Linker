fd69a9cdd9c14478b5e379a40175d8b9046b9890
BananeGuimauve <florian.gourdeau@gmail.com>
2017-01-06 10:41:48 +0100

    table_symbole.c et table_symbole.h finalisées et fonctionnelles

--- src/table_symbole.h
+++ src/table_symbole.h
@@ -3,5 +3,7 @@
 
 int creer_table_symbole(ELF_STRUCT * elf);
 void afficher_table(ELF_STRUCT * elf);
+void afficher_type(unsigned char st_info);
+void afficher_bind(unsigned char st_info);
 
 #endif
