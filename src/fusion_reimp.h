c38a0c7a008a154f7448e7cf7a0f76d239383651
BananeGuimauve <florian.gourdeau@gmail.com>
2017-01-11 11:13:57 +0100

    fusion_reimp.c et fusion_reimp.h probablement finis et en phase de tests

--- src/fusion_reimp.h
+++ src/fusion_reimp.h
@@ -1,8 +1,14 @@
 #ifndef __FUSION_REIMP_H__
 #define __FUSION_REIMP_H__
 
+/*	Décale l'offset de toutes les sections qui se trouvent après l'offset de la 
+	section d'indice passé en paramètre, d'une taille "size"	*/
 void offset_section_update(int ind_sect, Elf32_Word size, ELF_STRUCT * elf);
+
+//	Renvoie l'indice de la section d'offset maximum
 int max_offset_section(ELF_STRUCT * elf);
-void fusion_reimp(ELF_STRUCT * elf_file1, ELF_STRUCT * elf_file2)
+
+//	Programme principale de fusion des tables de réimplantation de 2 fichiers objets
+void fusion_reimp(ELF_STRUCT * elf_file1, ELF_STRUCT * elf_file2);
 
 #endif
